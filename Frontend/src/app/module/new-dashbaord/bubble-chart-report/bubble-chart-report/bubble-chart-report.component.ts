731ed4334b771fad8398d80973de4a4eb4ce9e7a
PiseyPhal <36697905+PiseyPhal@users.noreply.github.com>
2021-03-08 09:47:06 +0700

    Update bubble chart

--- Frontend/src/app/module/new-dashbaord/bubble-chart-report/bubble-chart-report/bubble-chart-report.component.ts
+++ Frontend/src/app/module/new-dashbaord/bubble-chart-report/bubble-chart-report/bubble-chart-report.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-bubble-chart-report',
@@ -14,24 +13,21 @@ export class BubbleChartReportComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  data = [
+
+  // List all project into bubble chart.
+  private data = [
     [[28604,77,17096869,'Australia',1990, 'red'],[26424,75.7,57110117,'United Kingdom',1990, 'black'],[37062,30,252847810,'United States',1990, 'bule']],
   ];
 
+  // Add project's data into bubble chart
   public bubbleChartOptions = { 
-    title: {
-      text: 'Bubble Chart'
-    },
-    grid: {
-      left: '6%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
+    title: { text: 'Bubble Chart' },
+    grid: { left: '6%', right: '4%', bottom: '3%', containLabel: true },
+    legend: { enabled: false },
     tooltip: {
       formatter: function (param) {
         return '<div style="border-bottom: 1px solid rgba(255,255,255,.3); font-size: 14px;padding-bottom: 2px;margin-bottom: 2px">'
-          + param.data[4]
+          + param.data[3] + " in " + param.data[4]
           + '</div>'
           + 'Total Investment' + '：' +  param.data[0] + '<br>'
           + 'Number of Projects' + '：' + param.data[1] + '<br>';
@@ -42,31 +38,24 @@ export class BubbleChartReportComponent implements OnInit {
       nameGap: 35,
       nameLocation: 'middle',
       splitLine: { show: false },
-      axisLabel: { 
-        formatter: '{value}B',
-        interval: 0,
-        rotate: 30 
-      }
+      axisLabel: { formatter: '{value}B', interval: 0, rotate: 30 },
     },
     yAxis: {
       name: "Number of Projects",
       nameGap: 35,
       nameLocation: 'middle',
       splitLine: { show: false },
-      // scale: true
     },
-    series: [{
-      type: 'scatter',
-      data: this.data[0],
-      symbolSize: function (data) {
-        return Math.sqrt(data[2]) / 5e2;
-      },
-      itemStyle: {
-        shadowBlur: 10,
-        shadowColor: 'rgba(120, 36, 50, 0.5)',
-        shadowOffsetY: 5,  
+    series: [
+      {
+        type: 'scatter',
+        data: this.data[0],
+        symbolSize: function (data) {
+          return Math.sqrt(data[3]) / 5e2;
+        },
+        itemStyle: { shadowBlur: 10, shadowColor: 'rgba(120, 36, 50, 0.5)', shadowOffsetY: 5},
       }
-    }],
+    ],
     animationDurationUpdate: 1000,
     animationEasingUpdate: 'quinticInOut'
   }
